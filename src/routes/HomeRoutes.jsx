49a5bbb27b22cadcb03e8e5fae4d21886328e5f5
Pham Hoang Anh <144643446+anhph2008@users.noreply.github.com>
2024-10-11 09:33:52 +0700

    page createOrder

--- src/routes/HomeRoutes.jsx
+++ src/routes/HomeRoutes.jsx
@@ -1,11 +1,18 @@
 import { lazy } from "react";
 import Loadable from './../components/Loadable';
+import CreateOrder from "pages/order/createOrder";
 
 const HomePage = Loadable(lazy(() => import('./../pages/HomePage')));
 
 const HomeRoutes = {
     path: '/',
-    element: <HomePage />
+    element: <HomePage />,
+    children: [
+        {
+          path: "create-order",
+          element: <CreateOrder />,
+        },
+    ]
 }
 
 export default HomeRoutes;
